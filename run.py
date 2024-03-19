9cb7f2867148a827ed231c9a9b4f7de72df7a30a
Chris Moynan <Chrismoynan@outlook.com>
2024-03-19 18:21:25 +0000

    added list for the words and a function that randomises the words

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
+words = ["apple", "banana", "orange", "strawberry", "grape", "pineapple", "watermelon", "mango", "melon", "cranberry"]
 
+def choose_word(words):
+    """
+    Randomly chooses a word from the array to begin the game.
+    """
+    return random.choice(words)
