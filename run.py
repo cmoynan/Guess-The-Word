63c9e21829c0b92a4b35a094064c234c780864ad
Chris Moynan <Chrismoynan@outlook.com>
2024-03-23 08:27:39 +0000

    added a main function. fixed game ending errors

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import random
 
 words = ["apple", "banana", "orange", "strawberry", "grape", "pineapple", "watermelon", "mango", "melon", "cranberry"]
+guessed_words = []
 
 def choose_word(words):
     """
@@ -9,13 +10,16 @@ def choose_word(words):
     chosen_word = random.choice(words)
     words.remove(chosen_word)
     return chosen_word
-  
+
 def play_game(words):
     """
     Main game function.
     """
+    if not words:
+        print("\n\033[31mNo words left to choose from. Game over.\033[0m")
+        return False  # Return False to indicate the game ends
 
-    print("\n\033[33mWelcome to Guess the Word!\033[0m")
+    print("\033[33mWelcome to Guess the Word!\033[0m")
     print("\nTry to guess the word within 6 attempts.")
     print("You only lose an attempt if the attempt is \033[31mincorrect\033[0m")
     print("\n\033[33mHint: Each word is a type of fruit\033[0m")
@@ -24,10 +28,6 @@ def play_game(words):
 
     guessed_letters = set()
     attempts = 6
-
-    if not words:
-        print("\n\033[31mNo words left to choose from. Game over.\033[0m")
-        return False
 
     while attempts > 0:
         """
@@ -41,45 +41,46 @@ def play_game(words):
         print("Attempts left:", attempts)
 
         while True:
-            guess = input("Guess a letter: ").lower()
+            guess = input("\nGuess a letter: ").lower()
             if len(guess) == 1 and guess.isalpha():
                 break
             else:
                 print("\n\033[31mError: Please enter only 1 letter and only a letter\033[0m")
 
         if guess in guessed_letters:
-            print("\n\033[31mYou already guessed that letter!\033[0m")
+            print("\033[31mYou already guessed that letter!\033[0m")
         elif guess in chosen_word:
-            print("\n\n\033[32mCorrect guess!\033[0m")
+            print("\n\033[32mCorrect guess!\033[0m")
             # Add the guessed letter to the set of guessed letters.
             guessed_letters.add(guess)
             if set(guessed_letters) == set(chosen_word):
                 # If the set of guessed letters is equal to the set of letters in the chosen word:
-                print("\033[32m\nCongratulations! You guess the word correctly. The word was:", chosen_word, "\033[0m")
-                return
+                print("\n\033[32mCongratulations! You guessed the word correctly. The word was:", chosen_word, "\033[0m")
+                guessed_words.append(chosen_word)
+                return True  # Return True to indicate the game ends
 
         else:
             print("\n\033[31mIncorrect guess!\033[0m")
             attempts -= 1
     
-    if not restart_game():
-        return
-        
-def restart_game():
-    """
-    Asks the player if they want to play again and returns True if yes, False otherwise.
-    """
-    while True:
-        play_again = input("\nDo you want to play again? (y/n): ").lower()
-        if play_again == 'y':
-            play_game(words)
-        elif play_again == 'n':
-            print("\nThanks for playing. We hope to see you again soon")
-            return
-        else:
-            print("\nInvalid input. Please enter 'y' to play again or 'n' to quit.")   
-                     
+    print("\n\033[31mSorry, you have run out of attempts. The word was:", chosen_word, "\033[0m\n")
+    return True  # Return True to indicate the game ends
 
-choose_word(words)
-play_game(words)
-restart_game()    
+def main():
+    global words
+    correct_guesses = 0
+    while True:
+        if not play_game(words):
+            score = len(guessed_words)
+            print("\n\033[33mYou guessed", score, "words correctly.\033[0m")
+            if not words:
+                print("\n\033[33mThanks for playing. We hope to see you again soon\033[0m")
+                break
+            play_again = input("\nDo you want to play again? (y/n): ").lower()
+            if play_again != 'y':
+                print("\033[33mThanks for playing. We hope to see you again soon\033[0m")
+                break
+            words = ["apple", "banana", "orange", "strawberry", "grape", "pineapple", "watermelon", "mango", "melon", "cranberry"]
+            guessed_words.clear()
+
+main()  
